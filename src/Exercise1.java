320e94073fa9c8a3a66cc33537cd0f373f065b2e
Andrea De Masi <andreademasi1999@gmail.com>
2023-10-09 15:23:49 +0200

    Exercise 1 done

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,15 +1,42 @@
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
         int[] myArr = new int[5];
         Random rndm = new Random();
+        Scanner input = new Scanner(System.in);
+
 
         for (int i = 0; i < myArr.length; i++) {
             myArr[i] = rndm.nextInt(1, 10);
         }
-//        System.out.println(Arrays.toString(myArr));
+        System.out.println(Arrays.toString(myArr));
 
+        sostituzioneArray:
+        while (true) {
+            for (int i = 0; i < myArr.length; i++) {
+                try {
+                    System.out.println("Scegli un numero da mettere nell'array, quando ti sei stancato premi 0");
+                    int number = Integer.parseInt(input.nextLine());
+                    if (number == 0) {
+                        break sostituzioneArray;
+                    } else {
+                        myArr[i] = number;
+                    }
+
+                } catch (NumberFormatException ex) {
+                    System.err.println(ex);
+                    System.err.println("Errore nell'inserimento dei numeri, sono ammessi solo numeri interi non lettere!");
+                    i--;
+                } finally {
+                    System.out.println(Arrays.toString(myArr));
+                }
+            }
+        }
 
     }
+
+
 }
